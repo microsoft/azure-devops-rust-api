276e698c731ca453693e3800bab1ecbdad2338dd
Manoj <myadav@microsoft.com>
2022-08-27 15:50:19 -0400

    Changed code to accept repo_name and pr_id as a CLI arguments

--- azure_devops_rust_api/examples/git_pr_commits.rs
+++ azure_devops_rust_api/examples/git_pr_commits.rs
@@ -28,8 +28,12 @@ async fn main() -> Result<()> {
 
     // Get ADO server configuration via environment variables
     let organization = env::var("ADO_ORGANIZATION").expect("Must define ADO_ORGANIZATION");
-    let repository_id = env::var("ADO_REPO_ID").expect("ADO_REPO_ID");
-    let pull_request_id: i32 = env::var("ADO_PR_ID").expect("ADO_PR_ID").parse().unwrap();
+    let repository_name = env::args()
+        .nth(1)
+        .expect("Usage: git_pr_commits <repository-name>");
+    let pull_request_id: i32 = env::args()
+        .nth(2)
+        .expect("Usage: git_pr_commits <pull_request_id>").parse().unwrap();
     let project = env::var("ADO_PROJECT").expect("Must define ADO_PROJECT");
 
     // Create a git client
@@ -38,7 +42,7 @@ async fn main() -> Result<()> {
     // Get Commits for the PR
     let pr_commits = git_client
         .pull_request_commits_client()
-        .get_pull_request_commits(&organization, &repository_id, pull_request_id, &project)
+        .get_pull_request_commits(&organization, &repository_name, pull_request_id, &project)
         .into_future()
         .await?;
     println!("{:#?}", pr_commits);
