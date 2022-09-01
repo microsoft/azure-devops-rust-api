7ff6054e6c8427e2d90a494918bf0c330d6f7f8d
Manoj <myadav@microsoft.com>
2022-09-01 19:41:12 -0400

    typo fix

--- azure_devops_rust_api/examples/git_pr_work_items.rs
+++ azure_devops_rust_api/examples/git_pr_work_items.rs
@@ -41,7 +41,7 @@ async fn main() -> Result<()> {
     // Create a "git" client
     let git_client = git::ClientBuilder::new(credential).build();
 
-    // Get work item(s) associted with PR
+    // Get work item(s) associated with PR
     let pr_work_items = git_client
         .pull_request_work_items_client()
         .list(&organization, &repository_name, pull_request_id, &project)
