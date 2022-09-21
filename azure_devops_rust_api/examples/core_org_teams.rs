9e2eeab4768b4d09baa6a0b1cccee10073abc20f
Manoj <myadav@microsoft.com>
2022-09-21 14:14:11 -0400

    added example to get teams name in the project

---
+++ azure_devops_rust_api/examples/core_org_teams.rs
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// core_org_teams.rs
+// Project Teams from Orgs example.
+use anyhow::Result;
+use azure_devops_rust_api::core;
+use azure_devops_rust_api::Credential;
+use std::env;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize logging
+    env_logger::init();
+
+    // Get authentication credential either from a PAT ("ADO_TOKEN") or via the az cli.
+    let credential = match env::var("ADO_TOKEN") {
+        Ok(token) => {
+            println!("Authenticate using PAT provided via $ADO_TOKEN");
+            Credential::from_pat(token)
+        }
+        Err(_) => {
+            println!("Authenticate using Azure CLI");
+            Credential::from_token_credential(Arc::new(azure_identity::AzureCliCredential {}))
+        }
+    };
+
+    // Get ADO server configuration via environment variables
+    let organization = env::var("ADO_ORGANIZATION").expect("Must define ADO_ORGANIZATION");
+
+    let project = env::var("ADO_PROJECT").expect("Must define ADO_PROJECT");
+
+    // Max number of teams to be fetched, default max is 100
+    let top_teams: i32 = 500;
+
+    // Create core client
+    let core_client = core::ClientBuilder::new(credential).build();
+
+    let org_teams = core_client
+        .teams_client()
+        .get_teams(&organization, &project)
+        .top(top_teams)
+        .into_future()
+        .await?
+        .value;
+
+    println!("\nProject teams name are :");
+
+    // Get teams names in the projects
+    for team in org_teams.iter() {
+        match &team.web_api_team_ref.name{
+            Some(name) => println!("{}", name),
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
