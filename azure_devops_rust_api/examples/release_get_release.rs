b4c8b9b007c26e4674cd026f411f7fe810909938
Manoj <myadav@microsoft.com>
2022-10-07 15:10:39 -0400

    fmt fix

--- azure_devops_rust_api/examples/release_get_release.rs
+++ azure_devops_rust_api/examples/release_get_release.rs
@@ -31,12 +31,14 @@ async fn main() -> Result<()> {
     let release_client = release::ClientBuilder::new(credential).build();
 
     // Get the release ID from user
-    let release_id:i32 = env::args()
-    .nth(1)
-    .expect("Usage: release_get_specific_release <release_id>").parse().unwrap();
+    let release_id: i32 = env::args()
+        .nth(1)
+        .expect("Usage: release_get_specific_release <release_id>")
+        .parse()
+        .unwrap();
 
     // Query a specific release
-    println!("\nReleases:");
+    println!("\nRelease:");
     let release = release_client
         .releases_client()
         .get_release(&organization, &project, release_id)
