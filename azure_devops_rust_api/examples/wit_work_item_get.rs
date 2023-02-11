7fdc6c51c00434922dc53057257216c1cda982d3
John Batty <johnbatty@microsoft.com>
2023-02-11 11:17:26 +0000

    More work item example cleanup. Clippy fixes.

--- azure_devops_rust_api/examples/wit_work_item_get.rs
+++ azure_devops_rust_api/examples/wit_work_item_get.rs
@@ -3,7 +3,7 @@
 
 // wit_work_item_get.rs
 // Work Item query example.
-use anyhow::Result;
+use anyhow::{anyhow, Context, Result};
 use azure_devops_rust_api::wit;
 use azure_devops_rust_api::wit::models::WorkItemRelation;
 use std::env;
@@ -13,9 +13,17 @@ mod utils;
 // For info on work item link types see:
 // https://learn.microsoft.com/en-us/azure/devops/boards/queries/link-type-reference?view=azure-devops
 const CHILD_RELATION_TYPE: &str = "System.LinkTypes.Hierarchy-Forward";
+const PARENT_RELATION_TYPE: &str = "System.LinkTypes.Hierarchy-Reverse";
+const RELATED_RELATION_TYPE: &str = "System.LinkTypes.Related";
 
-fn work_item_id_from_url(url: &str) -> i32 {
-    url.rsplit('/').next().unwrap().parse::<i32>().unwrap()
+// Extract work item id from url.
+// Work item url is of the form: https://dev.azure.com/.../<id>/_apis/wit/workItems/<work-item-id>
+fn work_item_id_from_url(url: &str) -> Result<i32> {
+    url.rsplit('/')
+        .next()
+        .ok_or_else(|| anyhow!("Failed to extract last segment of URL: {url}"))?
+        .parse::<i32>()
+        .with_context(|| format!("Failed to parse work item id from url: {url}"))
 }
 
 fn work_item_relations(work_item: &wit::models::WorkItem, relation_type: &str) -> Vec<i32> {
@@ -23,14 +31,14 @@ fn work_item_relations(work_item: &wit::models::WorkItem, relation_type: &str) -
         .relations
         .iter()
         .filter(|relation| relation.link.rel == relation_type)
-        .map(|related_work_item| work_item_id_from_url(&related_work_item.link.url))
+        .filter_map(|relation| work_item_id_from_url(&relation.link.url).ok())
         .collect()
 }
 
 fn relation_name(relation: &WorkItemRelation) -> String {
     relation.link.attributes["name"]
         .as_str()
-        .unwrap()
+        .unwrap_or("<unknown>")
         .to_string()
 }
 
@@ -61,17 +69,33 @@ async fn main() -> Result<()> {
         .expand("All")
         .await?;
 
-    println!("{:#?}", work_item);
+    println!("Work item [{work_item_id}]:\n{:#?}", work_item);
 
     // Show child work items
     let children = work_item_relations(&work_item, CHILD_RELATION_TYPE);
-    println!("\n{} child work items found:", children.len());
-    for child in children.iter() {
-        println!("  {}", child);
-    }
+    println!(
+        "\n[{work_item_id}] {} children: {:#?}",
+        children.len(),
+        children
+    );
+
+    // Show parent work items (expect to see only 0 or 1)
+    let parent = work_item_relations(&work_item, PARENT_RELATION_TYPE);
+    println!("\n[{work_item_id}] {} parent: {:#?}", parent.len(), parent);
+
+    // Show related work items
+    let related = work_item_relations(&work_item, RELATED_RELATION_TYPE);
+    println!(
+        "\n[{work_item_id}] {} related: {:#?}",
+        related.len(),
+        related
+    );
 
     // Show all work item relations
-    println!("\nAll relations:");
+    println!(
+        "\n[{work_item_id}] All {} relations:",
+        work_item.relations.len()
+    );
     for relation in work_item.relations.iter() {
         println!(
             "  {:30} {:40} {}",
