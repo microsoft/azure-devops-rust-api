c8108466416f192b9953a1d80a0013f1c1cf3d71
Manoj <myadav@microsoft.com>
2023-03-02 19:45:46 -0500

    lint and typo fix

--- azure_devops_rust_api/examples/test_plan.rs
+++ azure_devops_rust_api/examples/test_plan.rs
@@ -20,8 +20,10 @@ async fn main() -> Result<()> {
     // Get ADO server configuration via environment variables
     let organization = env::var("ADO_ORGANIZATION").expect("Must define ADO_ORGANIZATION");
     let project = env::var("ADO_PROJECT").expect("Must define ADO_PROJECT");
-    // let test_plan_id_value = env::var("TEST_PLAN_ID").expect("Must define PLAN_ID for the test");
-    // let test_plan_id:i32 = test_plan_id_value.parse().expect("Failed to parse TEST_PLAN_ID");
+    let test_plan_id_value = env::var("TEST_PLAN_ID").expect("Must define PLAN_ID for the test");
+    let test_plan_id: i32 = test_plan_id_value
+        .parse()
+        .expect("Failed to parse TEST_PLAN_ID");
     // Create test_plan client
     let test_plan_client = test_plan::ClientBuilder::new(credential).build();
 
@@ -35,16 +37,16 @@ async fn main() -> Result<()> {
     println!("{:#?}", test_plans);
 
     // Get all suites for a test plan for a project
-    // println!("The suites for the a plan for project are:");
-    // let test_suites = test_plan_client
-    //     .test_suites_client()
-    //     .get_test_suites_for_plan(&organization, &project, test_plan_id)
-    //     .await?
-    //     .value;
-    // println!("{:#?}", test_suites);
+    println!("The suites for the a plan for project are:");
+    let test_suites = test_plan_client
+        .test_suites_client()
+        .get_test_suites_for_plan(&organization, &project, test_plan_id)
+        .await?
+        .value;
+    println!("{:#?}", test_suites);
 
     // Get all test plan variables for project
-    println!("The test plan variables for project are:");
+    println!("The test plans variables for project are:");
     let test_plan_variables = test_plan_client
         .variables_client()
         .list(&organization, &project)
