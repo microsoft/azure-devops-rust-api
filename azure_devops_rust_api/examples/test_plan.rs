597966eecb521417ebe004b4fd575bacf58ec99a
MANOJ KUMAR YADAV <110555003+myadav27@users.noreply.github.com>
2023-03-03 03:37:38 -0500

    added code example to get test plan variables. (#126)
    * added code example to get test plan variables
    
    * add example for test suites and test plan configuration
    
    * test run
    
    * lint and typo fix
    
    ---------
    
    Co-authored-by: Manoj <myadav@microsoft.com>

--- azure_devops_rust_api/examples/test_plan.rs
+++ azure_devops_rust_api/examples/test_plan.rs
@@ -20,18 +20,48 @@ async fn main() -> Result<()> {
     // Get ADO server configuration via environment variables
     let organization = env::var("ADO_ORGANIZATION").expect("Must define ADO_ORGANIZATION");
     let project = env::var("ADO_PROJECT").expect("Must define ADO_PROJECT");
-
+    let test_plan_id_value = env::var("TEST_PLAN_ID").expect("Must define PLAN_ID for the test");
+    let test_plan_id: i32 = test_plan_id_value
+        .parse()
+        .expect("Failed to parse TEST_PLAN_ID");
     // Create test_plan client
     let test_plan_client = test_plan::ClientBuilder::new(credential).build();
 
     // Get all test plans for project
-    println!("The test runs for project are:");
+    println!("The test plan for project are:");
     let test_plans = test_plan_client
         .test_plans_client()
         .list(&organization, &project)
         .await?
         .value;
     println!("{:#?}", test_plans);
+
+    // Get all suites for a test plan for a project
+    println!("The suites for the a plan for project are:");
+    let test_suites = test_plan_client
+        .test_suites_client()
+        .get_test_suites_for_plan(&organization, &project, test_plan_id)
+        .await?
+        .value;
+    println!("{:#?}", test_suites);
+
+    // Get all test plan variables for project
+    println!("The test plans variables for project are:");
+    let test_plan_variables = test_plan_client
+        .variables_client()
+        .list(&organization, &project)
+        .await?
+        .value;
+    println!("{:#?}", test_plan_variables);
+
+    // Get all test plan configuration for project
+    println!("The test plan configuration for project are:");
+    let test_plan_configuration = test_plan_client
+        .configurations_client()
+        .list(&organization, &project)
+        .await?
+        .value;
+    println!("{:#?}", test_plan_configuration);
 
     Ok(())
 }
