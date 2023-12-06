7489537927f8142dc90a313a7e473c4a6c5a42c3
Kyle Rader <126627085+kyle-rader-msft@users.noreply.github.com>
2023-12-06 13:49:50 -0800

    Generate windows build scripts with GH CoPilot (#297)
    Co-authored-by: Kyle Rader <kyrader@microsoft.com>

---
+++ build.ps1
@@ -0,0 +1,34 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# Exit on error
+$ErrorActionPreference = "Stop"
+
+# Fetch the vsts-rest-api-specs submodule
+Write-Host "Fetch vsts-rest-api-specs"
+git submodule init
+git submodule update
+
+# Patch the API
+Write-Host "Create vsts-rest-api-specs.patched"
+Remove-Item -Recurse -Force vsts-rest-api-specs.patched -ErrorAction Ignore
+Set-Location vsts-api-patcher
+cargo run --release
+Set-Location ..
+
+# Autogen the Rust crate
+Write-Host "Autogen Rust crate"
+Remove-Item -Recurse -Force azure_devops_rust_api/target -ErrorAction Ignore
+Set-Location autorust/codegen
+cargo run --release
+Set-Location ../..
+
+# Format and build Rust crate
+Set-Location azure_devops_rust_api
+Write-Host "Format azure_devops_rust_api"
+cargo fmt
+Write-Host "Build azure_devops_rust_api"
+cargo build --all-features
+Set-Location ..
+
+Write-Host "Done"
